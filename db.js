ec2a7e6fb15c186be5523d5238ca9ec5ba21d557
alex-bartleynees <alexbartleynees@gmail.com>
2021-04-02 10:13:52 +1300

    Add getAlldishes and get dish functions

--- db.js
+++ db.js
@@ -3,4 +3,28 @@ const config = require('./knexfile')
 const env = process.env.NODE_ENV || 'development'
 const connection = knex(config[env])
 
+function getAllDishes (db = connection) {
+    return db('dishes')
+    .select()
+    .catch((err) => {
+        console.log(err.message)
+    })
+}
 
+function getDish (id, db = connection) {
+    return db('dishes')
+    .where('id', id)
+    .select()
+    .first()
+    .catch((err) => {
+        console.log(err.message)
+    })
+
+}
+
+
+module.exports = {
+    getAllDishes: getAllDishes,
+    getAllDishes: getDish,
+    
+}
