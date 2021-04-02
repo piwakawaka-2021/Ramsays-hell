323a7c648305731c924807fad38eb7cc9afb8ef3
alex-bartleynees <alexbartleynees@gmail.com>
2021-04-02 15:00:09 +1300

    update image urls

--- seeds/dish.js
+++ seeds/dish.js
@@ -6,11 +6,11 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('dish').insert([
-        {id: 8001, name: 'Vegetable Vegan Curry', time_tocook: 30,  imageURL: 'veg-curry.jpg' },
-        {id: 8002, name: 'Chicken Parmigiana', time_tocook: 60, imageURL: 'Chicken-Parmesam.jpg' },
-        {id: 8003, name: 'Spaghetti Bolognese', time_tocook: 30, imageURL: 'spag-bol.jpg'  },
-        {id: 8004, name: 'Burger', time_tocook: 20, imageURL: 'burger.jpg' },
-        {id: 8005, name: 'Salad', time_tocook: 10, imageURL: 'Salad.jpg' }
+        {id: 8001, name: 'Vegetable Vegan Curry', time_tocook: 30,  imageURL: '/images/veg-curry.jpg' },
+        {id: 8002, name: 'Chicken Parmigiana', time_tocook: 60, imageURL: '/images/Chicken-Parmesan.jpg' },
+        {id: 8003, name: 'Spaghetti Bolognese', time_tocook: 30, imageURL: '/images/spag-bol.jpg'  },
+        {id: 8004, name: 'Burger', time_tocook: 20, imageURL: '/images/burger.jpg' },
+        {id: 8005, name: 'Salad', time_tocook: 10, imageURL: '/images/Salad.jpg' }
       ]);
     });
 };
